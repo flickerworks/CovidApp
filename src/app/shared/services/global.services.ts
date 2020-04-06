389421898db212c9b175f42e954cae0adeb51f80
arpit.imcl@gmail.com <arpit.imcl@gmail.com>
2020-04-06 12:52:03 +0530

    removed unused

--- src/app/shared/services/global.services.ts
+++ src/app/shared/services/global.services.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LoggedInUserModel, MenuRoute, MonitorAndManagerList, UserModel, PersonalDetails, PatientDetails } from '../models/shared.model';
+import { LoggedInUserModel, MenuRoute, UserModel, PersonalDetails, PatientDetails } from '../models/shared.model';
 import { Subject } from 'rxjs';
 
 @Injectable()
